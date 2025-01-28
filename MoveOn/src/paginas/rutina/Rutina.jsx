a599529b88d8d1dc07d0d4d978108236bcd4f5e9
mangelccc <miguelangelgrimall@gmail.com>
2025-01-28 18:03:46 +0100

    Rutina mas o menos pero sin clean code

--- MoveOn/src/paginas/rutina/Rutina.jsx
+++ MoveOn/src/paginas/rutina/Rutina.jsx
@@ -1,13 +1,47 @@
 import React from "react";
-import "./Rutina.css"
+import "./Rutina.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faDumbbell,
+  faEgg,
+  faCalendar,
+  faPiggyBank,
+  faSmokingBan,
+  faBriefcase,
+  faCirclePlus,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Rutina = () => {
   return (
     <>
-      <h2>Esta sera la pagina de Deporte.</h2>
+      <div id="crear-rutina">
+        <div className="crear-rutina-general">
+          <FontAwesomeIcon icon={faCirclePlus} size="2x" />
+          <h2>Crear Rutina</h2>
+        </div>
+        <div className="contenedor-rutinas">
+          <div className="rutinas-item">
+            <FontAwesomeIcon icon={faDumbbell} size="5x" />
+          </div>
+          <div className="rutinas-item">
+            <FontAwesomeIcon icon={faEgg} size="5x" />
+          </div>
+          <div className="rutinas-item">
+            <FontAwesomeIcon icon={faCalendar} size="5x" />
+          </div>
+          <div className="rutinas-item">
+            <FontAwesomeIcon icon={faPiggyBank} size="5x" />
+          </div>
+          <div className="rutinas-item">
+            <FontAwesomeIcon icon={faSmokingBan} size="5x" />
+          </div>
+          <div className="rutinas-item">
+            <FontAwesomeIcon icon={faBriefcase} size="5x" />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
 
 export default Rutina;
-
