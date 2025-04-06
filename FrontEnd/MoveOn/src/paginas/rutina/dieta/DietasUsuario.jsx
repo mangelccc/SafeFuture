dfcef23fa0a058708796f77f878a8231b3a4bef3
lhachete <mhmonllor@hotmail.com>
2025-04-07 00:16:39 +0200

    Mostrar las dietas que tiene el usuario HECHO, a falta del diseño, también el del formulario, y sus posibles errores

--- FrontEnd/MoveOn/src/paginas/rutina/dieta/DietasUsuario.jsx
+++ FrontEnd/MoveOn/src/paginas/rutina/dieta/DietasUsuario.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import borrar from "../../../imagenes/borrar.png";
+import ver from "../../../imagenes/ver.png";
+import "./ListaTarjeta.css";
+
 
 const DietasUsuario = ({dieta}) => {
-    console.log(JSON.stringify(dieta));
-  return (
-    <div>DietasUsuario</div>
-  )
+    console.log(dieta);
+
+    return (
+      <section className='lista-tarjeta' /* id={} */
+        onClick={(evento) => {
+          if (evento.target.classList.contains("del")) {
+            //eliminarListaDeCompra(evento);
+          }
+        }}>
+        <div className='lista-tarjeta-izq'>
+          <section className='lista-tarjeta-nombre'>
+            <h3>{dieta.nombre}</h3>
+          </section>
+          <section className='lista-tarjeta-resumen'>
+            <div className="lista-tarjeta-contenido">
+              <p>Tu objetivo:</p>
+              <p>{dieta.pivot.objetivo} {dieta.pivot.objetivo === "Ganar" ? "masa" : "peso"}</p>
+            </div>
+            <div className="lista-tarjeta-contenido">
+              <p>Descripción:</p>
+              <p>{dieta.descripcion}</p>
+            </div>
+
+          </section>
+          
+        </div>
+        <div className="lista-tarjeta-der">
+
+          <Link> {/* Ejemplo en listas (UseParams) -> /listas-compra/${lista.id_lista} */}
+            <img src={ver} alt='Ver' title="Ver contenido de la lista" />
+          </Link>
+          <img src={borrar} className='del' alt='Borrar' title="Borrar la lista actual" />
+        </div>
+      </section>
+    );
 }
 
 export default DietasUsuario;
