5f668e4ac531cfd5d6ecee27ccc6ccbed90e49b1
mangelccc <miguelangelgrimall@gmail.com>
2025-05-08 16:50:21 +0200

    Acomodado rutinas

--- FrontEnd/MoveOn/src/paginas/rutina/ItemRutina.jsx
+++ FrontEnd/MoveOn/src/paginas/rutina/ItemRutina.jsx
@@ -11,7 +11,7 @@ const ItemRutina = ({ icono, link, title }) => {
         className="rutinas-item group relative w-64 h-64 flex justify-center items-center
           border-2 border-black dark:border-white2 rounded-xl text-gold dark:text-white bg-purple dark:bg-black overflow-hidden
           transition-all duration-300 dark:hover:text-gold hover:bg-purple dark:hover:bg-purpleOp hover:dark:border-turq hover:scale-102 hover:cursor-pointer">
-        <FontAwesomeIcon icon={icono} size="5x" />
+        <FontAwesomeIcon icon={icono} size="7x" />
 
         <div
           className=" absolute inset-0 bg-black bg-opacity-60 flex items-center justify-center opacity-0 transition-opacity duration-300 group-hover:opacity-100 hsm:opacity-0">
