e192cfcd7b683c3bff579d6ae11eb4a424b02e5b
mangelccc <miguelangelgrimall@gmail.com>
2025-05-12 00:45:56 +0200

    mejora css

--- FrontEnd/MoveOn/src/paginas/rutina/ejercicio/Plantillas.jsx
+++ FrontEnd/MoveOn/src/paginas/rutina/ejercicio/Plantillas.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React from 'react';
+import Entrenamiento from './Entrenamiento.jsx';
+import useAppContext from "../../../hooks/useAppContext.jsx";
 
 const Plantillas = () => {
+  const { entrenamientoContexto } = useAppContext();
+  const { entrenamientosFiltrados } = entrenamientoContexto;
   return (
-    <>
-      <h2>Bades dedatos ramon galinsoga based adasdsafbsafdsafsafsa</h2>
-    </>
+    <div className='p-6'>
+      <h2 className='dark:text-white mb-8'>
+        <strong>Plantillas entrenamientos</strong>
+      </h2>
+      <div className='w-full gap-4 h-[600px] overflow-y-scroll border-2 border-black dark:border-white rounded'>
+      {entrenamientosFiltrados.length > 0 ? (
+        entrenamientosFiltrados.map(ent => (
+          <Entrenamiento
+            key={ent.id_rutina}
+            entrenamiento={ent}
+          />
+        ))
+      ) : (
+        <p className="col-span-full text-center text-gray-500">
+          No hay entrenamientos.
+        </p>
+      )}
+      </div>
+    </div>
   );
 };
 
 export default Plantillas;
-
