2fb342fa8c15a4055fcbf756a3c824d9c4357ba9
lhachete <mhmonllor@hotmail.com>
2025-05-18 21:55:00 +0200

    fixed no-fumar

--- FrontEnd/MoveOn/src/contextos/NoFumarContexto.jsx
+++ FrontEnd/MoveOn/src/contextos/NoFumarContexto.jsx
@@ -43,14 +43,7 @@ export const NoFumarContexto = ({ children }) => {
 
 
     function getLocalISOString() {
-        const now = new Date();
-        // getTimezoneOffset devuelve minutos que hay que restar a UTC → en Madrid aprox -120
-        const tzOffsetMs = now.getTimezoneOffset() * 60 * 1000;
-        // restamos el offset para “deshacer” la conversión a UTC
-        const localTime = new Date(now.getTime() - tzOffsetMs);
-        // toISOString() → '2025-05-09T16:30:45.123Z'
-        // slice(0,19) → '2025-05-09T16:30:45'
-        return localTime.toISOString().slice(0, 19);
+        return new Date().toISOString();
     }
 
     const comenzarIntento = async () => {
@@ -102,6 +95,7 @@ export const NoFumarContexto = ({ children }) => {
             });
 
         } catch (err) {
+            console.error("Error al iniciar intento:", err);
             Swal.fire("Error", err.message, "error");
         }
     };
